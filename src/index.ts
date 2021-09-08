8ecf2beffe552a85dad12d936ca5049386366884
npenke <16369152+ncpenke@users.noreply.github.com>
2021-09-08 11:17:27 -0700

    Attempt at simplifying interface

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { StrToStr } from './utils';
 import { CachedSheet } from './cached_sheet';
 
-export function init() {
+declare var global: typeof globalThis;
+
+export function init(missingTransactionPatterns: StrToStr[]) {
+  (global as any).missingTransactionPatterns = missingTransactionPatterns;
   SpreadsheetApp.getUi().createMenu('Tiller Extensions')
+    .addItem('Next Missing Category', 'nextMissingCategory')
     .addItem('Delete Matching Transactions', 'deleteMatchingTransactions')
     .addToUi();
 }
 
-export function deleteMatchingTransactions(transactionPatterns: StrToStr[]) {
-  let sheet = new CachedSheet(SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Transactions"), transactionPatterns);
+export function transactionsSheet()
+{
+  return SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Transactions");
+}
+
+export function deleteMatchingTransactions() {
+  let transactionPatterns: StrToStr[] = (global as any).missingTransactionPatterns;
+  let sheet = new CachedSheet(transactionsSheet(), transactionPatterns);
   let allValues = [...sheet.cachedValues]; // create a copy of the cached values since we'll be modifying them
   allValues = allValues.filter(e => !sheet.rowMatchesPattern(e));
   if (allValues.length != sheet.cachedValues.length) {
@@ -16,3 +26,33 @@ export function deleteMatchingTransactions(transactionPatterns: StrToStr[]) {
     SpreadsheetApp.getUi().alert(`Deleted ${sheet.cachedValues.length - allValues.length} transactions`);
   }
 }
+
+function findNextMissingCategory(sheet: CachedSheet)
+{
+  let allValues = [...sheet.cachedValues]; // create a copy of the cached values since we'll be modifying them
+  let catIndex = sheet.columnIndex("Category");
+  let idx = 0;
+  allValues.forEach(v => {
+    if (allValues[catIndex].length == 0)
+      return idx;
+    idx++;
+  })
+  return -1;
+}
+
+export function nextMissingCategory() {
+  let sheet = new CachedSheet(transactionsSheet(), []);
+  let rowIdx = findNextMissingCategory(sheet);
+  if (rowIdx < 0) {
+    SpreadsheetApp.getUi().alert("No rows are missing categories");
+  }
+  else {
+    let catIndex = sheet.columnIndex("Category");
+    let range = sheet.original.getRange(rowIdx, catIndex, 1, 1);
+    transactionsSheet().setActiveRange(range);
+  }
+}
+
+(global as any).deleteMatchingTransactions = deleteMatchingTransactions;
+(global as any).nextMissingCategory = nextMissingCategory;
+(global as any).missingTransactionPatterns = [];
